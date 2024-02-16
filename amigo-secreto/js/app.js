b40a868ca26f19e36d25f3fafa4fb274417eb38f
oswaldovzki <45914716+oswaldovzki@users.noreply.github.com>
2024-02-16 07:56:31 -0300

    Finalização do projeto de Sorteio de amigo oculto

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -2,23 +2,45 @@ var friendList = [];
 
 function adicionar() {
     var friendName = document.getElementById('nome-amigo').value;
-    friendList.push(friendName);
-
-
+    
+    if (friendName == '') {
+        alert('PAM!');
+    } else if (friendList.includes(friendName)) {
+        alert('O nome ja existe. insira também um sobre nome');
+    } else {
+        friendList.push(friendName);
+    }
+    
     //Adicionar nomes no campo
     var friendsName = document.getElementById('lista-amigos');
-    friendsName.innerHTML = friendList.sort();   
+    friendsName.innerHTML = friendList.sort();
 }
 
 function sortear() {
+    if (friendList.length < 3) {
+        alert('Quantidade invalida de integrantes. Necessario pelo menos 3 participantes.');
+        return;
+    }
+    
     embaralha(friendList);
+    var sorteio = document.getElementById('lista-sorteio');
 
-    console.log(friendList);
+    for (let i = 0; i < friendList.length; i++) {
+        if (i == friendList.length -1) {
+            sorteio.innerHTML = sorteio.innerHTML + friendList[i] + ' --> ' + friendList[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + friendList[i] + ' --> ' + friendList[i + 1] + '<br>';
+        }
+    }
 }
 
 
 function reiniciar() {
-    location.reload()
+    friendList = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('nome-amigo').value = '';
+    // location.reload();
 }
 
 function embaralha(lista) {
@@ -28,7 +50,7 @@ function embaralha(lista) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
         // atribuição via destructuring
-        [lista[indice - 1], lista[indiceAleatorio]] = 
+        [lista[indice - 1], lista[indiceAleatorio]] =
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
